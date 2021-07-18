1c240ba91968f817757f84bdbaa4ad4d6b9204f8
Aiden Isaia <aidenisaia@gmail.com>
2021-07-18 14:57:58 -0700

    showing quotes and attempting to sort my theme

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <h2 v-for="quote in quotes" v-bind:key="quote.id">
+      <p>"{{ quote.quote }}"</p>
+      <p>{{ quote.source }}</p>
+      <p>{{ quote.context }}</p>
+      <hr />
+    </h2> -->
+    <button v-on:click="sortByGame">Quotes by game</button>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      quotes: [],
+      gamequotes: [],
+      moviequotes: [],
+    };
+  },
+  created: function () {
+    axios
+      .get("https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json")
+      .then((response) => {
+        console.log(response.data);
+        this.quotes = response.data;
+      });
+  },
+  methods: {
+    sortByGame: function (array) {
+      array.forEach(function (quote) {
+        if (quote["theme"] == "games") {
+          this.gamequotes.push(quote);
+        }
+      });
+    },
   },
 };
 </script>
